970601c396116fd0c169e387bd68ca8c7bfe2098
zhj <18230159792@163.com>
2019-09-15 16:27:36 +0800

    hello_dev

--- src/main/java/day0915/Hello.java
+++ src/main/java/day0915/Hello.java
@@ -2,6 +2,7 @@ package day0915;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("zhjhello_update");
+
+        System.out.println("zhjhello_update_dev");
     }
 }
